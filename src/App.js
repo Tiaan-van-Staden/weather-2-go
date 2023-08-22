003a42990383fc00b3aec970eed5bdb55646926a
Tiaan van Staden <tiaan.vanstaden@4-sure.net>
2023-08-22 12:38:09 +0200

    setup-cleanup-initial starting point with all func

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-3xl font-semibold text-red-400'>Hello world</h1>
+      <UilReact size={50}/>
     </div>
   );
 }
